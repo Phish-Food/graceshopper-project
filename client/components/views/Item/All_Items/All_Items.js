330f97d066cfe96714d32a5531f0e045d6c6ca92
MoAdam10 <meedoadam@gmail.com>
2021-08-04 15:49:04 -0400

    allitems and singleitem components

--- client/components/views/Item/All_Items/All_Items.js
+++ client/components/views/Item/All_Items/All_Items.js
@@ -1,14 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { connect } from "react-redux";
-import { StyledAll_Items } from "./All_Items.styled";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { StyledAll_Items } from './All_Items.styled';
+import { fetchAllItemsThunk } from '../../../../redux/reducers/item';
+import Item_Item from '../Item_Item/Item_Item';
 
-const All_Items = (props) => {
-  return <StyledAll_Items></StyledAll_Items>;
+const All_Items = ({ items, getAllItems }) => {
+	useEffect(() => {
+		getAllItems();
+	}, [items.length]);
+	console.log('items', items);
+	return (
+		<StyledAll_Items>
+			{items.map((item) => {
+				return <Item_Item key={item.id} item={item} />;
+			})}
+		</StyledAll_Items>
+	);
+};
+const mapStateToProps = (state) => {
+	const { items } = state.item;
+	return {
+		items,
+	};
 };
 
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getAllItems: () => dispatch(fetchAllItemsThunk()),
+	};
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(All_Items);
--- client/components/views/Item/Item_Item/Item_Item.js
+++ client/components/views/Item/Item_Item/Item_Item.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { connect } from "react-redux";
-import { StyledItem_Item } from "./Item_Item.styled";
+import React from 'react';
+import { connect } from 'react-redux';
+import { StyledItem_Item } from './Item_Item.styled';
+import { Link } from 'react-router-dom';
 
-const Item_Item = (props) => {
-  return <StyledItem_Item></StyledItem_Item>;
+const Item_Item = ({ item }) => {
+	return (
+		<StyledItem_Item>
+			<img src={item.imageUrl} />
+			<ul>
+				<li>
+					<Link to={`/items/${item.id}`}>{item.name}</Link>
+				</li>
+				<li>{item.price}</li>
+				<li>{item.description}</li>
+			</ul>
+		</StyledItem_Item>
+	);
 };
 
 const mapStateToProps = (state) => ({});
