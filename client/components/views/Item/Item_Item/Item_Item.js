1ac03d5164da2110c5a75b5e4b0dbc11b9378d09
AndrewBloodworth <andrewcbld@gmail.com>
2021-08-05 15:32:24 -0600

    Added Virtual field to Item for price in dollars

--- client/components/views/Item/Item_Item/Item_Item.js
+++ client/components/views/Item/Item_Item/Item_Item.js
@@ -1,38 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import { StyledItem_Item } from './Item_Item.styled';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { StyledItem_Item } from "./Item_Item.styled";
+import { Link } from "react-router-dom";
 
 const Item_Item = ({ item }) => {
-	const getAverage = (reviews) => {
-		if (!reviews.length) {
-			return 0;
-		}
-		return (
-			reviews.reduce((rating, review) => {
-				return review.rating + rating;
-			}, 0) / reviews.length
-		);
-	};
-	return (
-		<StyledItem_Item>
-			<Link to={`/items/${item.id}`}>
-				<header>
-					<section id="item-section">
-						<img src={item.imageUrl} />
-					</section>
-				</header>
-				<div>
-					<h3>{item.name}</h3>
+  const getAverage = (reviews) => {
+    if (!reviews.length) {
+      return 0;
+    }
+    return (
+      reviews.reduce((rating, review) => {
+        return review.rating + rating;
+      }, 0) / reviews.length
+    );
+  };
+  return (
+    <StyledItem_Item>
+      <Link to={`/items/${item.id}`}>
+        <header>
+          <section id="item-section">
+            <img src={item.imageUrl} />
+          </section>
+        </header>
+        <div>
+          <h3>{item.name}</h3>
 
-					<p>Price: {item.price}</p>
-					<p>Description: {item.description}</p>
-					<p>Reviews: {getAverage(item.reviews)}</p>
-				</div>
-			</Link>
-		</StyledItem_Item>
-	);
+          <p>Price: {item.dollars}</p>
+          <p>Description: {item.description}</p>
+          <p>Reviews: {getAverage(item.reviews)}</p>
+        </div>
+      </Link>
+    </StyledItem_Item>
+  );
 };
 
 const mapStateToProps = (state) => ({});
