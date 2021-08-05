e64c007923ad931983bce23941c29d209c92b170
Doug Henderson <dhend595@gmail.com>
2021-08-05 17:32:26 -0400

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,20 +4,20 @@ const config = require("./config");
 const pkg = require("../../package.json");
 const db = new Sequelize(`postgres://localhost:5432/${pkg.name}`);
 
-// const db = new Sequelize({
-//   database: config.database.name,
-//   username: config.database.username,
-//   password: config.database.password,
-//   host: config.database.host,
-//   port: config.database.port,
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+const db = new Sequelize({
+  database: config.database.name,
+  username: config.database.username,
+  password: config.database.password,
+  host: config.database.host,
+  port: config.database.port,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 
 
